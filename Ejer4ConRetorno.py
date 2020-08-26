c2baac1c637ef54c7fe3ff9a7236fc72b9a53488
hjosemejia <hjosemejia@uniguajira.edu.co>
2020-08-26 13:03:15 -0500

    version 1

---
+++ Ejer4ConRetorno.py
@@ -0,0 +1,24 @@
+import math #importamos la libreria math para la funcion pow
+
+def funcion_serie(num1):
+    sum = 1 + num1
+    y = 2
+    
+    for i in range(1,50): 
+        fct = 1
+        for j in range(1,y+1): 
+            fct = fct * j          
+        m = math.pow(num1, y) / fct 
+        sum = sum + m 
+        y += 1    
+    return sum
+ 
+
+def main():
+    num1 = int(input("DIGITE UN NUMERO: "))
+    serie = funcion_serie(num1)
+    print(f"EL VALOR DE LA FUNCION ESPONECIAL CON X = {num1} Y CON N = 50 ")
+    print(f"e^X = {serie}")
+
+if __name__ == "__main__":
+    main()
